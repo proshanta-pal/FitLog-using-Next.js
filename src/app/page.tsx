d309963aeb1c5010f305557654bfd5297e746aa4
proshanta-pal <proshanta.pal13@gmail.com>
2026-09-23 20:50:12 +0600

    feat/complete fetching data, adding types, and WorkoutCard components with responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Banner from "@/components/homepage/Banner";
-import Image from "next/image";
+import Workout from "@/components/homepage/Workout";
 
 export default function Home() {
   return (
     <div className="my-10 mx-5">
       <Banner />
+      <Workout />
     </div>
   );
 }
---
+++ src/components/homepage/Workout.tsx
@@ -0,0 +1,26 @@
+import { getWorkoutData } from '@/lib/apps'
+import React from 'react'
+import WorkoutCard from '../shared/WorkoutCard';
+import { IExercise } from '@/types/workout.type';
+
+export default async function Workout() {
+
+  const workoutData = await getWorkoutData();
+  console.log(workoutData);
+
+  return (
+    <main className='max-w-7xl mx-auto mt-12'>
+        <div className='space-y-3'>
+            <h2 className='uppercase text-5xl'>THe Library</h2>
+            <p className='text-xl font-medium text-[#B5B7AD]'>Twelve lifts covering every major muscle group.</p>
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-7 gap-5'>
+            {
+                workoutData.map((exercise: IExercise) => <WorkoutCard key={exercise.id} exercise={exercise}/>)
+            }
+        </div>
+
+    </main>
+  )
+}
